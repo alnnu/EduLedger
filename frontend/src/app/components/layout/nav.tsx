217e30c8f06734e7b6dd6d713ca3d3cfc4c3c716
alnnu <launnf1020@gmail.com>
2025-10-31 11:16:53 -0300

    add: navbar

--- frontend/src/app/components/layout/nav.tsx
+++ frontend/src/app/components/layout/nav.tsx
@@ -1,1 +1,108 @@
+import { Menu } from "lucide-react";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
 
+
+
+
+const TopMenu = [
+  { name: "Criar certificado", href: "/#" },
+  { name: "certificados", href: "/#" },
+];
+
+const Logo = () => {
+  return (
+    <Link href="/" className="flex space-x-2 py-3 items-center">
+      <h1 className="text-xl font-bold flex items-center gap-2">
+        <Image
+          src="/favicon.ico"
+          alt="logo"
+          width={32}
+          height={32}
+        />
+        EduLedger
+      </h1>
+    </Link>
+  );
+};
+
+
+export default function Navbar() {
+
+  return (
+    <header className="py-4 px-8 z-50 container">
+      <div className=" justify-between min-w-full mx-3 md:mx-0">
+        <nav className="hidden justify-between md:flex">
+          <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2">
+              <Logo />
+            </div>
+          </div>
+          <div className="items-center flex gap-6">
+            <div className="flex items-center">
+              {TopMenu.map((menu, idx) => (
+                <a
+                  key={idx}
+                  className={cn(
+                    "text-muted-foreground",
+                    navigationMenuTriggerStyle,
+                    buttonVariants({ variant: "ghost" })
+                  )}
+                  href={menu.href}
+                >
+                  {menu.name}
+                </a>
+              ))}
+            </div>
+          </div>
+        </nav >
+
+        {/* Mobile Menu */}
+        < div className="block md:hidden" >
+          <div className="flex items-center justify-between">
+            <Logo />
+            <Sheet>
+              <div className="flex items-center">
+                <SheetTrigger asChild>
+                  <Button variant={"outline"} size={"icon"}>
+                    <Menu className="size-4" />
+                  </Button>
+                </SheetTrigger>
+              </div>
+              <SheetContent className="overflow-y-auto px-4">
+                <SheetHeader className="p-0 pt-4">
+                  <SheetTitle>
+                    <div className="flex items-center">
+                      <Logo />
+                    </div>
+                  </SheetTitle>
+                </SheetHeader>
+                <div className="my-4 flex flex-col gap-0">
+                  {TopMenu.map((menu, idx) => (
+                    <a
+                      key={idx}
+                      href={menu.href}
+                      className="font-semibold text-lg py-2"
+                    >
+                      {menu.name}
+                    </a>
+                  ))}
+                </div>
+              </SheetContent>
+            </Sheet>
+          </div>
+        </div >
+      </div >
+    </header >
+  );
+}
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import Navbar from "./components/layout/nav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
